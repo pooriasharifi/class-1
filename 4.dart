786bac67401c0615581721d69d4f6872380de6a5
zn-gostaran <zn.gostaran@gmail.com>
2022-08-24 13:57:21 +0430

    sjnswd

--- 4.dart
+++ 4.dart
@@ -1,11 +1,14 @@
 import 'dart:io';
 void main (){
 
-print ("عدد را وارد کنید");
-String? num=stdin.readLineSync();
-int preNum= int.parse(num!);
-if (0<preNum!){print('prenum=$preNum');}
-else string? wenum = (-1*int.parse(num!));
- print("wenum=$wenum");
+ print ("عدد را وارد کنید");
+ String? num=stdin.readLineSync();
+ int preNum= int.parse(num!);
+ if (0<preNum!){print('prenum=$preNum');}
+ else {
+
+  print("wenum=${preNum*(-1)}");
+
+ }
 
 }
